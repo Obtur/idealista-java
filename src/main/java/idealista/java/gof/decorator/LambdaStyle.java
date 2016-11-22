61b7c87f501e7100fbc0118bd59aef6a36eff5f4
Juan David Vega <jdvr.93@hotmail.com>
2016-11-22 17:39:04 +0100

    decorator

---
+++ src/main/java/idealista/java/gof/decorator/LambdaStyle.java
@@ -0,0 +1,13 @@
+package idealista.java.gof.decorator;
+
+public class LambdaStyle {
+
+    public static void main(String[] args) {
+        //general, regional, health
+        System.out.println(
+                ""
+        );
+    }
+
+
+}
