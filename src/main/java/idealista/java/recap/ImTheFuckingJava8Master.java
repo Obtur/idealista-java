703ea7bfcfb5e756bda17d071d4af4fcddea8efa
acuervof <acuervof@gmail.com>
2016-11-30 00:12:44 +0100

    sesion2

---
+++ src/main/java/idealista/java/recap/ImTheFuckingJava8Master.java
@@ -0,0 +1,49 @@
+package idealista.java.recap;
+
+import idealista.java.transform.Ad;
+import idealista.java.transform.User;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Created by acuervo on 29/11/16.
+ */
+public class ImTheFuckingJava8Master {
+
+    public double sumPriceOfAds(List<Ad> ads) {
+        return ads.stream()
+                    .mapToDouble(Ad::getPrice)
+                    .sum();
+    }
+
+
+    public List<User> findAllDiferentUsersWithNameJuan(List<Ad> ads) {
+        return ads.stream()
+                .map(Ad::getUser)
+                .filter(u -> u.getName().equals("Juan"))
+                .distinct()
+                .collect(Collectors.toList());
+    }
+
+    public Ad findTheOldestAd(List<Ad> ads) {
+        return ads.stream()
+                .min((a1, a2) -> a1.getPublicationDate().compareTo(a2.getPublicationDate()))
+                .get();
+    }
+
+
+    public List<String> findAllPhonesForHousesBeingSold(List<Ad> ads) {
+        return ads.stream()
+                .filter(a -> a.getOperation().equals(Ad.Operation.Sale))
+                .map(a -> a.getUser().getPhone())
+                .distinct()
+                .collect(Collectors.toList());
+    }
+
+    public List<Ad> findTowAdsWithSamePrice(List<Ad> ads) {
+        return ads.stream()
+                .
+    }
+}
--- src/test/java/idealista/java/recap/IKnowHowToUseStreamsIf.java
+++ src/test/java/idealista/java/recap/IKnowHowToUseStreamsIf.java
@@ -18,6 +18,8 @@ import static org.junit.Assert.assertThat;
 
 public class IKnowHowToUseStreamsIf {
 
+    private ImTheFuckingJava8Master imTheFuckingJava8Master = new ImTheFuckingJava8Master();
+
 	private static final List<Ad> ads = asList(
 			new Ad(new User("Juan", "34928411238"), LocalDate.of(2012, 6, 23), Typology.House, Operation.Sale, 300000d),
 			new Ad(new User("Juan", "34628421238"), LocalDate.of(2014, 5, 2), Typology.Garage, Operation.Rent, 50d),
@@ -29,14 +31,14 @@ public class IKnowHowToUseStreamsIf {
 
 	@Test
 	public void i_can_sum_the_price_of_all_the_ads() {
-		double totalPrice = 0;
+		double totalPrice = imTheFuckingJava8Master.sumPriceOfAds(ads);
 
 		assertThat(totalPrice, is(closeTo(300896d, 0.0001)));
 	}
 
 	@Test
 	public void i_can_find_all_different_users_with_name_Juan() {
-		List<User> users = new ArrayList<>();
+		List<User> users = imTheFuckingJava8Master.findAllDiferentUsersWithNameJuan(ads);
 
 		assertThat(users, containsInAnyOrder(
 				new User("Juan", "34928411238"),
@@ -47,7 +49,7 @@ public class IKnowHowToUseStreamsIf {
 
 	@Test
 	public void i_can_find_the_oldest_ad() {
-		Ad ad = null;
+		Ad ad = imTheFuckingJava8Master.findTheOldestAd(ads);
 		Ad expectedAd = new Ad(new User("Juan", "33917211238"), LocalDate.of(1996, 9, 21), Typology.House, Operation.Rent, 300d);
 
 		assertThat(ad, is(expectedAd));
@@ -55,7 +57,7 @@ public class IKnowHowToUseStreamsIf {
 
 	@Test
 	public void i_can_find_all_the_phones_for_houses_being_sold() {
-		List<String> users = new ArrayList<>();
+		List<String> users = imTheFuckingJava8Master.findAllPhonesForHousesBeingSold(ads);
 
 		assertThat(users, containsInAnyOrder(
 				"34928411238",
@@ -65,7 +67,7 @@ public class IKnowHowToUseStreamsIf {
 
 	@Test
 	public void i_can_find_two_ads_with_the_same_price() {
-		List<Ad> adsWithSamePrice = new ArrayList<>();
+		List<Ad> adsWithSamePrice = imTheFuckingJava8Master.findTowAdsWithSamePrice(ads);
 
 		assertThat(adsWithSamePrice, containsInAnyOrder(
 				new Ad(new User("Rubén", "34928411238"), LocalDate.of(2011, 1, 3), Typology.Office, Operation.Sale, 23d),
